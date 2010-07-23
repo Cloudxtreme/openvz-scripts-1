1a0f37c1ccd46175b7f3e691d6de6134b210e2d9
Paul Berens <pharaun666@gmail.com>
2010-07-22 23:06:33 -0400

    dumb vzlist setup, need to find a more intelligent way

--- vzlist.rb
+++ vzlist.rb
@@ -13,24 +13,99 @@ module OpenVZ
 
 	    # Define all of the "output format" for each function
 	    @status = 'ctid,hostname,status,laverage,cpulimit,cpuunits,ip'
-	end
+	    @kmemsize = mblf('kmemsize')
+	    @lockedpages = mblf('lockedpages')
+	    @privvmpages = mblf('privvmpages')
+	    @shmpages = mblf('shmpages')
+	    @numproc = mblf('numproc')
+	    @physpages = mblf('physpages')
+	    @vmguarpages = mblf('vmguarpages')
+	    @oomguarpages = mblf('oomguarpages')
+	    @numtcpsock = mblf('numtcpsock')
+	    @numflock = mblf('numflock')
+	    @numpty = mblf('numpty')
+	    @numsiginfo = mblf('numsiginfo')
 
-	def status
+	    @tcpsndbuf = mblf('tcpsndbuf')
+	    @tcprcvbuf = mblf('tcprcvbuf')
+	    @othersockbuf = mblf('othersockbuf')
+	    @dgramrcvbuf = mblf('dgramrcvbuf')
+	    @numothersock = mblf('numothersock')
+	    @dcachesize = mblf('dcachesize')
+	    @numfile = mblf('numfile')
+	    @numiptent = mblf('numiptent')
+
+	    @diskspace = sh('diskspace')
+	    @diskinodes = sh('diskinodes')
+	end
+	
+	def all
 	    puts ssh_exec(@status)
 	end
 
+	def all
+	    puts ssh_exec(@status)
+	    puts ssh_exec(@kmemsize)
+	    puts ssh_exec(@lockedpages)
+	    puts ssh_exec(@privvmpages)
+	    puts ssh_exec(@shmpages)
+	    puts ssh_exec(@numproc)
+	    puts ssh_exec(@physpages)
+	    puts ssh_exec(@vmguarpages)
+	    puts ssh_exec(@oomguarpages)
+	    puts ssh_exec(@numtcpsock)
+	    puts ssh_exec(@numflock)
+	    puts ssh_exec(@numpty)
+	    puts ssh_exec(@numsiginfo)
+	    puts ssh_exec(@tcpsndbuf)
+	    puts ssh_exec(@tcprcvbuf)
+	    puts ssh_exec(@othersockbuf)
+	    puts ssh_exec(@dgramrcvbuf)
+	    puts ssh_exec(@numothersock)
+	    puts ssh_exec(@dcachesize)
+	    puts ssh_exec(@numfile)
+	    puts ssh_exec(@numiptent)
+	    puts ssh_exec(@diskspace)
+	    puts ssh_exec(@diskinodes)
+	end
+
+	def open_ssh
+	    @session = Net::SSH.start(@host, @user)
+	end
+	
+	def close_ssh
+	    @session.close
+	end
 
 	private
 	def ssh_exec(output)
 	    stdout = ""
-	    Net::SSH.start(@host, @user) do |ssh|
-		# capture only stdout matching a particular pattern
-		ssh.exec!("sudo vzlist -a -o #{output}") do |channel, stream, data|
-		    stdout << data if stream == :stdout
-		end
+	    @session.exec!("sudo vzlist -a -o #{output}") do |channel, stream, data|
+		stdout << data if stream == :stdout
+	    end
+	    return stdout
+	end
+
+	def mblf(value)
+	    mblf = ['m', 'b', 'l', 'f']
+	    ret = [value]
+
+	    mblf.each do |suffix|
+		ret << "#{value}.#{suffix}"
 	    end
 
-	    return stdout
+	    return ret.join(',')
+	end
+	
+	def sh(value)
+	    sh = ['s', 'h']
+	    ret = [value]
+
+	    sh.each do |suffix|
+		ret << "#{value}.#{suffix}"
+	    end
+
+	    return ret.join(',')
 	end
     end
 end
